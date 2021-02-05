f374309cb30f69994fd54e7c86a128305ae19c05
yoshikyoto <stb_yoshiyuki-s@hotmail.co.jp>
2021-02-05 10:31:20 +0900

    Static Page Generation

--- pages/index.js
+++ pages/index.js
@@ -2,8 +2,9 @@ import Head from 'next/head'
 import Link from 'next/link'
 import Layout from "../components/layout";
 import utilStyles from '../styles/utils.module.css'
+import { getSortedPostsData } from '../lib/posts'
 
-export default function Home() {
+export default function Home({ allPostsData }) {
   return (
     <Layout home>
       <Head>
@@ -13,14 +14,29 @@ export default function Home() {
 
       <main>
         <section className={utilStyles.headingMd}>
-          <p>
-            Read {' '}
-            <Link href="/posts/first-post">
-              <a>this page</a>
-            </Link>
-          </p>
+          <h2 className={utilStyles.headingLg}>Blog</h2>
+          <ul className={utilStyles.list}>
+            {allPostsData.map(({ id, date, title }) => (
+              <li className={utilStyles.listItem} key={id}>
+                {title}
+                <br />
+                {id}
+                <br />
+                {date}
+              </li>
+            ))}
+          </ul>
         </section>
       </main>
     </Layout>
   )
+}
+
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData()
+  return {
+    props: {
+      allPostsData
+    }
+  }
 }
