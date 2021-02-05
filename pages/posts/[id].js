1cb1d36da21d6913fe8d1fe8a58442e0d8af6ad9
yoshikyoto <stb_yoshiyuki-s@hotmail.co.jp>
2021-02-05 13:47:36 +0900

    日付の表示をフォーマット

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,14 +1,20 @@
+import Head from 'next/head'
 import Layout from '../../components/layout'
 import { getAllPostIds, getPostData } from '../../lib/posts'
+import Date from '../../components/date'
 
 export default function Post({ postData }) {
   return (
     <Layout>
+      <Head>
+        <title>{postData.title}</title>
+      </Head>
+
       {postData.title}
       <br />
       {postData.id}
       <br />
-      {postData.date}
+      <Date dateString={postData.date} />
       <br />
       <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
     </Layout>
